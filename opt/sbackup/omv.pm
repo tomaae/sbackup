c4d5a56f9333385140157307174d092edbc60ca4
tomaae <23486452+tomaae@users.noreply.github.com>
2018-05-29 07:58:01 +0000

    omv, changed integration to with backed on sharefolder names omv, added debugs omv, added full path existence check backup, added new omv4 integration backup, added filesystem integration

--- opt/sbackup/omv.pm
+++ opt/sbackup/omv.pm
@@ -12,7 +12,7 @@ use warnings;
 
 use Exporter qw(import);
 our @ISA = qw(Exporter);
-our @EXPORT = qw(omv_name_by_uuid omv_mnt_by_uuid omv_path_by_uuid omv_is_mounted omv_prepare_uuid);
+our @EXPORT = qw(omv_prepare_sharedfolder);
 
 ##
 ## omv_name_by_uuid
@@ -46,19 +46,24 @@ sub omv_mnt_by_uuid {
 sub omv_path_by_uuid {
 	my $uuid = shift;
 	my $result = "";
-#      <sharedfolder>
-#        <uuid>c39e30a8-9201-4b0f-a848-07a5f18ec533</uuid>
-#        <name>backuptest</name>
-#        <comment></comment>
-#        <mntentref>f8092d74-045d-449b-b5b5-de11fdfdf7f2</mntentref>
-#        <reldirpath>backuptest/</reldirpath>
-#        <privileges></privileges>
-#      </sharedfolder>
-
-	#. /usr/share/openmediavault/scripts/helper-functions&&omv_config_get "//config/system/shares/sharedfolder[name='backuptest']"
 	$result = `. /usr/share/openmediavault/scripts/helper-functions&&omv_get_sharedfolder_path \"$uuid\"`;
 	if($? != 0 || $result eq ""){
 		$result = "Error:OMV path not found";
+	}else{
+		$result = "Error:OMV path does not exists" if !-d $result;
+	}
+	return $result;
+}
+
+##
+## omv_uuid_by_name
+##
+sub omv_uuid_by_name {
+	my $sharedfolder_name = shift;
+	my $result = "";
+	$result = `. /usr/share/openmediavault/scripts/helper-functions&&omv_config_get \"/config/system/shares/sharedfolder[name=\'$sharedfolder_name\']/uuid\"`;
+	if($? != 0 || $result eq ""){
+		$result = "Error:OMV failed to get uuid";
 	}
 	return $result;
 }
@@ -77,20 +82,25 @@ sub omv_is_mounted {
 }
 
 ##
-## omv_prepare_uuid
+## omv_prepare_sharedfolder
 ##
-sub omv_prepare_uuid {
-	my $uuid = shift;
+sub omv_prepare_sharedfolder {
+	my $sharedfolder_name = shift;
 	my $result = "";
-	&::f_output("DEBUG","Preparing OMV uuid \"$uuid\"");
+	&::f_output("DEBUG","Preparing OMV sharedfolder \"$sharedfolder_name\"");
+	##Get uuid
+	my $uuid = omv_uuid_by_name($sharedfolder_name);
+	&::f_output("DEBUG","sharedfolder UUID \"$uuid\"");
 	##Get mount point
 	my $uuid_mnt = omv_mnt_by_uuid($uuid);
+	&::f_output("DEBUG","sharedfolder mnt \"$uuid_mnt\"");
 	return $uuid_mnt if $uuid_mnt =~ /^Error:/;
 	##Verify if mounted
 	$result = omv_is_mounted($uuid_mnt);
 	return $result if $result =~ /^Error:/;
 	##Get path
 	my $uuid_path = omv_path_by_uuid($uuid);
+	&::f_output("DEBUG","sharedfolder path \"$uuid_path\"");
 	return $uuid_path if $uuid_path =~ /^Error:/;
 	
 	$result = $uuid_path;
