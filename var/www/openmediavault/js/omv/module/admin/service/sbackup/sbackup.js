12787115f16034a79a1f28d1fa129a34c4c9ba7e
tomaae <23486452+tomaae@users.noreply.github.com>
2018-06-28 21:15:15 +0200

    removed omv2 files

--- var/www/openmediavault/js/omv/module/admin/service/sbackup/sbackup.js
+++
@@ -1,7 +0,0 @@
-OMV.WorkspaceManager.registerNode({
-	id: "sbackup", // Individual ID
-	path: "/service", // Parent folder in the navigation view
-	text: _("Sync Backup"), // Text to show next to the icon
-	icon16: "images/hdd.png", // 16x16 pixel icon that is displayed in the navigation view
-	iconSvg: "images/hdd.svg" // SVG icon that is displayed in the navigation view
-});
